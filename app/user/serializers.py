123dee3455104c956915d89944d8bd8408564b7e
gabspaludo <gpf.gabriel.gpf@gmail.com>
2025-07-24 14:48:45 -0300

    Fix typo

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -44,5 +44,5 @@ class AuthTokenSerializer(serializers.Serializer):
             msg = _('Unable to authenticate with provided credentials')
             raise serializers.ValidationError(msg, code='authorization')
 
-        attrs ['user'] = user
+        attrs['user'] = user
         return attrs
