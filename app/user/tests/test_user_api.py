7db659ed7e465cff7a1e41d19543bd66c3325d7a
gabspaludo <gpf.gabriel.gpf@gmail.com>
2025-07-25 10:29:46 -0300

    Fix tests for manage user API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -140,11 +140,12 @@ class PrivateUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_update_user_profile(self):
-        """Test updating the user profile for the unauthenticated user."""
+        """Test updating the user profile for the authenticated user."""
         payload = {'name': 'Updated name', 'password': 'newpassword123'}
 
         res = self.client.patch(ME_URL, payload)
 
         self.user.refresh_from_db()
-        self.assertAlmostEqual(self.user.name, payload['password'])
-        self.assertTrue(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
